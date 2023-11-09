0e2b9b5f58c74063b94a6933d28729d49b514e69
NARENDIR <97464705+NARENDIR@users.noreply.github.com>
2023-11-09 15:52:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Define the Docker image name and tag
-IMAGE_NAME=project
-TAG="latest"
-
-# Build the Docker image using the provided Dockerfile
-docker build -t $IMAGE_NAME:$TAG .
-
-# Display a message indicating the image has been built
-echo "Docker image $IMAGE_NAME:$TAG has been built successfully."
+docker build -t project .
+docker-compose down || true
+docker-compose up -d
