f955426a945c20d4d8d2be07e4c55f59967b3486
NARENDIR <97464705+NARENDIR@users.noreply.github.com>
2023-11-09 14:49:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ $GIT_BRANCH=="origin/dev"]]; then 
+if [[ $GIT_BRANCH == "origin/dev" ]]; then 
  ./build.sh
  
   docker login -u narendiranr2 -p dckr_pat_325C7oay9o3gjdQyyZz9d0npX2Y
@@ -9,7 +9,7 @@ if [[ $GIT_BRANCH=="origin/dev"]]; then
 #Push the image to the Dev Docker Hub repository docker
  push narendiranr2/dev
 
-elif [[ $GIT_BRANCH "origin/main" ]]; then 
+elif [[ $GIT_BRANCH == "origin/main" ]]; then 
  ./build.sh
  docker login -u narendiranr2 -p dckr_pat_325C7oay9o3gjdQyyZz9d0npX2Y
  docker tag project narendirr2/prod
