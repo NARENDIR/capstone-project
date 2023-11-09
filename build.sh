25db37a158a8dd2290cf6cbca3f743125a060ca7
NARENDIR <97464705+NARENDIR@users.noreply.github.com>
2023-11-09 12:07:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 if [[ $GIT_BRANCH== "origin/dev"]]; then 
  ./build.sh
+ 
 docker login -u narendiranr2 -p dckr_pat_325C7oay9o3gjdQyyZz9d0npX2Y
+
 #Tag the image 
 docker tag project narendiranr2/dev
 #Push the image to the Dev Docker Hub repository docker
 push narendiranr2/dev
+
 elif [[ $GIT_BRANCH "origin/main" ]]; then 
 ./build.sh
 docker login -u narendiranr2 -p dckr_pat_325C7oay9o3gjdQyyZz9d0npX2Y
